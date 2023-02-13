d2581969a77cf481b678583105dbf96b070da287
Jean-Claude Jung <jjung@adobe.com>
2023-02-13 18:32:46 +0100

    changed mimetype to list of image file extension

--- src/filecheck.py
+++ src/filecheck.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
-import mimetypes
 import sys
 from pathlib import Path
 
+WHITELIST_EXT = [
+    'png',
+    'jpeg',
+    'jpg'
+    'gif',
+    'tiff',
+    'bmp',
+    'psd',
+    'heic'
+]
+
 image_files = []
 for f in sys.argv[1::]:
-    mt, n = mimetypes.guess_type(f)
-    ext = Path(f).suffix
-    if ext.lower() == '.heic' or (mt != None and "image" in mt):
+    ext = Path(f).suffix.replace('.', '').lower()
+    if ext in WHITELIST_EXT:
         image_files.append(f)
 
 files = "\t".join(image_files)
