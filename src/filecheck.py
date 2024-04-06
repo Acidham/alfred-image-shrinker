2be85e7bef89df4810ca208774d0778b9c820498
Acidham <acidham@gmail.com>
2024-04-06 12:00:07 +0200

    add image suffix option

--- src/filecheck.py
+++ src/filecheck.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import os
+import shutil
 import sys
 from pathlib import Path
 
@@ -18,6 +20,15 @@ image_files = []
 for f in sys.argv[1::]:
     ext = Path(f).suffix.replace('.', '').lower()
     if ext in WHITELIST_EXT:
+        # create a copy if img_suffix is not empty in WF config
+        if os.getenv('img_suffix'):
+            path, filename = os.path.split(f)
+            basename, extension = os.path.splitext(filename)
+            new_file_name = f"{basename}{os.getenv('img_suffix')}.{extension}"
+            new_file = os.path.join(path, new_file_name)
+            shutil.copyfile(f, new_file)
+            f = new_file
+
         image_files.append(f)
 
 files = "\t".join(image_files)
